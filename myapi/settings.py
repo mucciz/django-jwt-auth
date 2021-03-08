719235c699979a25343fbc98269504c2c9b1b7ae
Kenneth Muchiri <kenmucciz8@gmail.com>
2021-03-09 01:24:58 +0300

    Added postgres

--- myapi/settings.py
+++ myapi/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'myapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'booktime',
+        'USER': 'deathstar',
+        'PASSWORD': 'deathstar',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
